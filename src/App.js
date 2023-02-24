162aa10845d8dd8a1f4010ed68da6cf793987f1f
Muhammad Noman <Noman@SS-04.sagacious.local>
2023-02-24 15:26:48 +0500

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Team from "./Components/Team/Team";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+         <Header/>
+         <Team/>
+         <Footer/>
+    </>
+  );
+}
+
+export default App;
